b70bedb284f084770e2f91d2fe621506658b5925
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-09 12:33:18 +0300

    Update implementation.py

--- day_1/flow_control/task_1/implementation.py
+++ day_1/flow_control/task_1/implementation.py
@@ -1,6 +1,16 @@
-def get_numbers():
+def get_numbers(start=1000, end=2000):
+    seq = []
     """Возвращает все числа от 1000 до 2000, которые делятся на 7, но не кратны 5
 
     Returns: итерируемый объект с нужными числами
     """
-    raise NotImplementedError
+    if end <= start:
+        print("Warning: End should to be greater than start")
+        
+    for number in range(start, end):
+        if number % 7 == 0 and number % 5 != 0:
+            seq.append(number)
+            
+    return seq
+
+ #   raise NotImplementedError

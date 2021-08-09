f30db9935fd268c0235920e9438bc4aea1287e14
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-09 16:02:52 +0300

    Update question.py

--- day_1/scope/task_3/question.py
+++ day_1/scope/task_3/question.py
@@ -1,14 +1,24 @@
 """
 Что будет выведено после выполнения кода? Почему?
 """
-x = 42
+def print_msg(number):
+
+    def printer():
+        nonlocal number
+        number=3
+        print(number)
+
+    printer()
+    print(number)
 
 
-def some_func():
-    global x
-    x = x + 1
-    print(x)
+print_msg(9)
 
-
-some_func()
-print(x)
+# Внутри print_msg() вызывается printer()
+# внутри printer() ссылаемся на number, который находится в области видимости print_msg
+# далее меняем на 3 и печатаем number. На печати будет 3.
+# после printer() идет стандартный print(). Так как значение number было перезаписано в printer(),
+# то также будет выведена тройка на печать
+# Итого:
+# 3
+# 3

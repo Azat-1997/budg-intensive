6598e7131cc1d1985d1ddf0bb7b783feb6d81c61
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-09 11:32:10 +0300

    Update question.py

--- day_1/scope/task_2/question.py
+++ day_1/scope/task_2/question.py
@@ -12,3 +12,8 @@ def transmit_to_space(message):
 
 
 print(transmit_to_space("Test message"))
+# transmit_to_space("Test message") - выведет "Test message"
+# т.к. message найдется в аргументах внешней функции.
+# Далее будет напечатано "None": если функция ничего не возвращает (не имеет return <something>),
+# то, по умоланию, возвращается None, который пропечатается внешним принтом
+

935f7ea51dfa10b18846d9e15439af7e73bd8e4f
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-09 22:32:39 +0300

    Update implementation.py

--- day_1/flow_control/task_3/implementation.py
+++ day_1/flow_control/task_3/implementation.py
@@ -10,12 +10,13 @@ def get_days_count_by_month(month):
     month31 = {"декабрь", "январь", "март", "май", "июль", "август", "октябрь"}
     month30 = {"апрель", "июнь", "сентябрь", "ноябрь"}
     febrary = "февраль"
+    result = 0
     if month in month30:
-        return 30
+        result = 30
     elif month in month31:
-        return 31
+        result = 31
     elif month == febrary:
-        return 28
-    else:
-        return 0
+        result = 28
+    
+    return result
         

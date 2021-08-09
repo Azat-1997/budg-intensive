5d13daef83b520a1ada4f7481c60d1713f386d4b
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-09 18:24:42 +0300

    Update tests.py

--- day_1/flow_control/task_4/tests.py
+++ day_1/flow_control/task_4/tests.py
@@ -10,16 +10,16 @@ from day_1.flow_control.task_4.implementation import (
 
 class MyTestCase(unittest.TestCase):
     def test_next_day(self):
-        self.assertEqual(get_next_date(date(2021, 3, 3)), date(2021, 3, 4))
+        self.assertEqual(get_next_date("2021-03-03"), "2021-03-04")
 
     def test_next_month(self):
-        self.assertEqual(get_next_date(date(2021, 4, 30)), date(2021, 5, 1))
+        self.assertEqual(get_next_date("2021-04-30"),  "2021-05-01")
 
     def test_next_year(self):
-        self.assertEqual(get_next_date(date(2021, 12, 31)), date(2022, 1, 1))
+        self.assertEqual(get_next_date("2021-12-31"), "2022-01-01")
 
     def test_next_extra_february(self):
-        self.assertEqual(get_next_date(date(2020, 2, 28)), date(2020, 2, 29))
+        self.assertEqual(get_next_date("2020-02-28"), "2020-02-29")
 
 
 if __name__ == '__main__':

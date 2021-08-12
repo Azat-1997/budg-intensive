d95893eb197b6ffeb24f1c9ba464c1c63da29173
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2021-08-12 20:12:51 +0300

    Add files via upload

---
+++ day_4/magic_method/task_1/implementation.py
@@ -0,0 +1,84 @@
+class Multiplier:
+
+    def __init__(self, value) -> None:
+        super().__init__()
+
+    def _evaluate(self, other, action):
+        # генерируем подходящее выражение, чтобы далее вычислять значения в магических методах
+        if isinstance(other, (Hundred, Thousand, Million)):
+            result = eval(str(self.get_value()) + action + str(other.get_value()))
+        elif isinstance(other, (int, float)):
+            result = eval(str(self.get_value()) + action + str(other))
+
+        else:
+            raise TypeError
+        
+        return result
+
+
+class Hundred(Multiplier):
+    """Множитель на 100"""
+    __SCALE = 100
+    def __init__(self, value):
+        self.__value = value * Hundred.__SCALE
+
+    def get_value(self):
+        return self.__value 
+
+    def __add__(self, other):
+        return Hundred(super()._evaluate(other, "+") / Hundred.__SCALE)
+        
+    def __sub__(self, other):
+        return Hundred(super()._evaluate(other, "-") / Hundred.__SCALE)
+
+    def __mul__(self, other):
+        return Hundred(super()._evaluate(other, "*") / Hundred.__SCALE)
+
+    def __truediv__(self, other):
+        return Hundred(super()._evaluate(other, "/") / Hundred.__SCALE)
+
+
+class Thousand(Multiplier):
+    """Множитель на 1 000"""
+    __SCALE = 1000
+    def __init__(self, value):
+        self.__value = value * Thousand.__SCALE
+
+    def get_value(self):
+        return self.__value
+
+    def __add__(self, other):
+        return Thousand(super()._evaluate(other, "+") / Thousand.__SCALE)
+
+    def __sub__(self, other):
+        return Thousand(super()._evaluate(other, "-") / Thousand.__SCALE)
+
+    def __mul__(self, other):
+        return Thousand(super()._evaluate(other, "*") / Thousand.__SCALE)
+
+    def __truediv__(self, other):
+        return Thousand(super()._evaluate(other, "/") / Thousand.__SCALE)
+
+   
+class Million(Multiplier):
+    """Множитель на 1 000 000"""
+    __SCALE = 1_000_000
+    def __init__(self, value):
+        self.__value = value * Million.__SCALE
+
+    def get_value(self):
+        return self.__value
+
+    def __add__(self, other):
+        return Million(super()._evaluate(other, "+") / Million.__SCALE)
+
+    def __sub__(self, other):
+        return Million(super()._evaluate(other, "-") / Million.__SCALE)
+
+    def __mul__(self, other):
+        return Million(super()._evaluate(other, "*") / Million.__SCALE)
+
+    def __truediv__(self, other):
+        return Million(super()._evaluate(other, "/") / Million.__SCALE)
+
+
